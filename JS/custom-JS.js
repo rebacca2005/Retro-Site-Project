0f69bcfebaf03ccc1f37939f8458e38d9fd80740
rebacca2005 <minrebacca@gmail.com>
2023-08-08 00:02:56 -0400

    first commit

---
+++ JS/custom-JS.js
@@ -0,0 +1,10 @@
+$(document).ready(function() 
+{
+
+  // This function will load global footer and header
+  $(function()
+  {
+    $('#footer1').load('footer.html');
+    $('#header1').load('header.html');
+  });
+});
